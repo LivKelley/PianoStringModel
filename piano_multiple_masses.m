edeb5be517fd714656c1c67eb106ceb339709279
nielsenlouise <louiseenielsen@gmail.com>
2015-11-24 14:54:10 -0500

    here is a file and i might have accidentally changed things but it shouldn't be important

--- piano_multiple_masses.m
+++ piano_multiple_masses.m
@@ -24,7 +24,7 @@ State = [m1_i, m2_i, m3_i, dm1_i, dm2_i, dm3_i];
 
 %% an ode function
 
-[T, Y] = ode45(@motion, [0,15], State);
+[T, Y] = ode45(@motion, [0,1], State);
 
 %% a motion function
 
@@ -36,10 +36,10 @@ State = [m1_i, m2_i, m3_i, dm1_i, dm2_i, dm3_i];
         dm2 = Info(5);
         dm3 = Info(6);
         
-        fs1 = -k * (sqrt(x^2 + m1^2) - x)*sign(m1);
-        fs2 = -k * (sqrt(x^2 + (m2-m1)^2) - x)*sign(m2-m1);
-        fs3 = -k * (sqrt(x^2 + (m3-m2)^2) - x)*sign(m3-m2);
-        fs4 = -k * (sqrt(x^2 + m3^2) - x)*sign(m3);
+        fs1 = -k * (sqrt(x^2 + m1^2) - x)*sign(m1)* m1 / sqrt(x^2 + m1^2);
+        fs2 = -k * (sqrt(x^2 + (m2-m1)^2) - x)*sign(m2-m1)* (m2-m1) / sqrt(x^2 + (m2-m1)^2);
+        fs3 = -k * (sqrt(x^2 + (m3-m2)^2) - x)*sign(m3-m2)* (m3-m2) / sqrt(x^2 + (m3-m2)^2);
+        fs4 = -k * (sqrt(x^2 + (-m3)^2) - x)*sign(m3)* m3 / sqrt(x^2 + m3^2);
         
         fm1 = fs1 - fs2;
         fm2 = fs2 - fs3;
@@ -52,8 +52,8 @@ State = [m1_i, m2_i, m3_i, dm1_i, dm2_i, dm3_i];
         res = [dm1; dm2; dm3; am1; am2; am3];
     end
 
-plot(T,Y(:,1));
-plot(T,Y(:,2));
-plot(T,Y(:,3));
+loglog(T,Y(:,1));
+loglog(T,Y(:,2));
+loglog(T,Y(:,3));
 
 end
