56f2eb01747f9d160b8a398adbc01b8ad055ebf4
nielsenlouise <louiseenielsen@gmail.com>
2015-12-09 22:36:40 -0500

    "Pretty animation!"

--- the_code_to_animate.m
+++ the_code_to_animate.m
@@ -6,7 +6,8 @@ pause;
 Dimensions = size(Result_matrix);
 end_loop = Dimensions(1);
 n = Dimensions(2);
-
+Vector = linspace(0, pi, n); 
+sineVector = [4*sin(Vector)]; 
 for j = 1:2000
     for i = 1:end_loop
         clf
@@ -14,6 +15,9 @@ for j = 1:2000
         axis([0 n -8 8]);
         drawnow;
     end
+    pause;
+    plot(sineVector);
+    axis([0 n -8 8]);
     pause;
 end
 
